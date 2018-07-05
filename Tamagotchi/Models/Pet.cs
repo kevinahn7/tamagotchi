3c3153223c1e580ac4c9efa27bdabbb04c656874
Kevin Ahn and Ryan Murry <kevin@>
2018-07-05 09:10:16 -0700

    update pet properties

--- Tamagotchi/Models/Pet.cs
+++ Tamagotchi/Models/Pet.cs
@@ -3,8 +3,40 @@ namespace Tamagotchi.Models
 {
     public class Pet
     {
-        public Pet()
+        private string _name;
+        private int _attention = 100;
+        private int _rest = 100;
+        private int _hunger = 100;
+        public int _age = 1;
+
+        public Pet(string name)
         {
+            _name = name;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public int GetAttention()
+        {
+            return _attention;
+        }
+
+        public int GetRest()
+        {
+            return _rest;
+        }
+
+        public int GetHunger()
+        {
+            return _hunger;
+        }
+
+        public int GetAge()
+        {
+            return _age;
         }
     }
 }
