fddce068d1f4af5f78c6fa6172509cad709fd24a
Kevin Ahn and Ryan Murry <ryan@>
2018-07-05 09:43:29 -0700

    Add UpdateAge method

--- Tamagotchi/Models/Pet.cs
+++ Tamagotchi/Models/Pet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Tamagotchi.Models
 {
     public class Pet
@@ -7,11 +8,16 @@ namespace Tamagotchi.Models
         private int _attention = 100;
         private int _rest = 100;
         private int _hunger = 100;
-        public int _age = 1;
+        private int _age = 1;
+        private int _time = 0;
+        private static List<Pet> _petList = new List<Pet>() { };
+        private int _id;
 
         public Pet(string name)
         {
             _name = name;
+            _id = _petList.Count;
+            _petList.Add(this);
         }
 
         public string GetName()
@@ -38,5 +44,46 @@ namespace Tamagotchi.Models
         {
             return _age;
         }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public void UpdateAttributes()
+        {
+            _rest -= 10;
+            _hunger -= 10;
+            _attention -= 10;
+            _time += 20;
+            UpdateAge();
+        }
+
+        public void UpdateAge()
+        {
+            if (_time % 100 == 0)
+            {
+                _age++;
+            }
+        }
+
+        public void UpdateAttention()
+        {
+            _attention += 30;
+            UpdateAttributes();
+        }
+
+        public void UpdateHunger()
+        {
+            _hunger += 30;
+            UpdateAttributes();
+        }
+
+        public void UpdateRest()
+        {
+            _rest += 30;
+            UpdateAttributes();
+        }
+
     }
 }
